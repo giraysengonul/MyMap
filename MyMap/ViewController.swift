2b74567229b07f80722bcfd0d244c1dd22cf603c
hakkicansengonul <coder.9734@gmail.com>
2022-10-16 07:54:54 +0300

    Initial Commit

---
+++ MyMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMap
+//
+//  Created by hakkı can şengönül on 16.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
